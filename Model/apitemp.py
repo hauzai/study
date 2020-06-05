ee2b0c3012cf6f0599e125c8caf34877591efca2
yaoyihua <yaoyihua@togest.com>
2020-06-05 17:50:14 +0800

    1.完善apitemp模块中Apttemp类的相关内容

--- Model/apitemp.py
+++ Model/apitemp.py
@@ -1,7 +1,48 @@
-import getdata
 import handledata
 
+from util import getdata
 class Apitemp:
-    def __init__(self,path,filename, sheetname, apiname):
-        getdata.get_datadict()
-    # TODO:api模板类，与testcase对象组合生成requestdict对象
+    def __init__(self, apidata: getdata.Getdata):
+        self.data = apidata.data
+
+        self.__api_data = {}
+        self.api_id = ""
+        self.handle_data()
+
+        self.request_data = {}
+        self.__temp = {'method': '方法', 'headers': 'headers', 'params': '入参', 'url': 'api'}
+
+    def handle_data(self):
+        for key, value in self.data.items():
+            self.api_id = key
+            self.__api_data = value
+
+    def __str__(self):
+        return self.api_id+":"+str(self.request_data)
+
+    def get_api_data(self):
+        return self.__api_data
+
+    def update_request_data(self, **kwargs):
+        for k, v in kwargs.items():
+            self.request_data[k] = v
+
+    def set_request_data(self, keys=['url', 'method', 'params', 'headers']):
+        for i in keys:
+            self.request_data[i] = self.__api_data[self.__temp[i]]
+
+    def get_apitemp(self):
+        return {self.api_id:self.request_data}
+
+
+
+if __name__ == '__main__':
+    test1 = getdata.Getdata(filename="常用接口文档.xlsx", sheetname="安全保障app", id="togest-001")
+    test = Apitemp(test1)
+    test.handle_data()
+    test.set_request_data()
+    print(test)
+    print(test.request_data)
+    print(test.get_apitemp())
+    test.update_request_data(headers={"token": "2100000000000000"})
+    print(test.request_data)
--- util/getdata.py
+++ util/getdata.py
@@ -6,50 +6,70 @@ data_path = os.path.join(current_path, r"..\data")
 
 class Getdata:
     def __init__(self, filename, sheetname, path=data_path, id=""):
+        """
+        :param filename: 获取文件名称
+        :param sheetname: 获取sheet名称
+        :param path: 文件所在目录
+        :param id: 获取数据id
+        """
         # 私有变量，获取文件夹路径、路径下文件名列表、文件sheet列表、sheet中的所有数据id列表
         self.__path = path
         self.__filenames = []
         self.__sheetnames = []
         self.__ids = []
+
         # 变量 解析的文件名、解析文件的sheet名、解析数据的id
         self.filename = filename
         self.sheetname = sheetname
         self.id = id
 
+        # 定义需返回的 datas 及 data 变量
         self.datas = {}
+        self.data = {}
+
         # 私有变量，解析文件的路径
         self.__filepath = os.path.join(self.__path, self.filename)
 
+        # 声明文件对象、table对象
         self.__workbook: xlrd.book.Book = None
         self.__table: xlrd.sheet.Sheet = None
 
+        # 获取文件所在目录的文件名称、获取文件对象、获取文件对象下的sheet名称、获取table对象
         self.setfilenames()
         self.get_file()
         self.setsheetnames()
         self.get_table()
 
-    def getdatas(self):
+        # 获取datas对象
+        self.__getdatas()
+
+        # 如果创建对象时传入了id则直接获取data
+        if self.id:
+            self.data = self.getdata(self.id)
+
+    def __getdatas(self):
         # TODO:处理文档中数据，原样输出
         """
-        获取数据表中数据，此处以描述为键，行内所有数据字典为值
+        获取数据表中数据，此处以id为键，行内所有数据字典为值
         :return:
         """
         if self.__table.nrows > 1:
             data_key = self.__table.row_values(0)
             for i in range(1, self.__table.nrows):
                 data_temp = dict(zip(data_key, self.__table.row_values(i)))
-                dict_key = data_temp['描述']
+                dict_key = data_temp['id']
                 self.datas[dict_key] = data_temp
                 self.__ids.append(dict_key)
-        print(self.datas)
+        # print(self.datas)
         # pass
 
     def getdata(self,id=""):
         # TODO:获取某特定的data
         if id:
-            return self.datas[id]
-        else:
-            return self.datas[self.id]
+            return {id: self.datas[id]}
+
+    def getdatas(self):
+        return self.datas
 
     def setfilenames(self):
         """
@@ -66,7 +86,7 @@ class Getdata:
         设置self.__sheetnames
         :return:
         """
-        self.__sheetnames =  self.__workbook.sheet_names()
+        self.__sheetnames = self.__workbook.sheet_names()
 
     def get_file(self):
         """
@@ -89,5 +109,7 @@ class Getdata:
 if __name__ == '__main__':
     # print(data_path)
     test = Getdata(filename="常用接口文档.xlsx", sheetname="安全保障app")
-    test.getdatas()
-    print(test)
+    test1 = Getdata(filename="常用接口文档.xlsx", sheetname="安全保障app", id="togest-001")
+    print(test1.data)
+    test2 = Getdata(filename="常用接口文档.xlsx", sheetname="test-测试用例")
+    print(test2.datas)
